7a66e12b4539ec9a45bc30487aaf861413126305
sherbek turdiyev <sherbekturdiyev98@gmail.com>
2022-03-09 14:05:20 +0500

    oxirgi

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,61 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <v-data-table
+    hide-default-footer
+    v-model="selected"
+    :headers="headers"
+    :items="desserts"
+    item-key="name"
+    show-select
+    class="elevation-1">
+    </v-data-table>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'AboutView',
+  data () {
+    return {
+      selected: [],
+      headers: [
+        {
+          text: 'Dessert (100g serving)',
+          align: 'start',
+          sortable: false,
+          value: 'name'
+        },
+        { text: 'Calories', value: 'calories' },
+        { text: 'Fat (g)', value: 'fat' },
+        { text: 'Carbs (g)', value: 'carbs' },
+        { text: 'Protein (g)', value: 'protein' },
+        { text: 'Iron (%)', value: 'iron' }
+      ],
+      desserts: [
+        { name: 'Frozen Yogurt', calories: 159, fat: 6.0, carbs: 24, protein: 4.0, iron: '1%' },
+        { name: 'Ice cream sandwich', calories: 237, fat: 9.0, carbs: 37, protein: 4.3, iron: '1%' },
+        { name: 'Eclair', calories: 262, fat: 16.0, carbs: 23, protein: 6.0, iron: '7%' },
+        { name: 'Cupcake', calories: 305, fat: 3.7, carbs: 67, protein: 4.3, iron: '8%' },
+        { name: 'Gingerbread', calories: 356, fat: 16.0, carbs: 49, protein: 3.9, iron: '16%' },
+        { name: 'jdhasi', calories: 356, fat: 16.0, carbs: 49, protein: 3.9, iron: '16%' },
+        { name: 'slkacm', calories: 356, fat: 16.0, carbs: 49, protein: 3.9, iron: '16%' },
+        { name: ' oqwi9', calories: 356, fat: 16.0, carbs: 49, protein: 3.9, iron: '16%' },
+        { name: 'HYW', calories: 356, fat: 16.0, carbs: 49, protein: 3.9, iron: '16%' },
+        { name: 'qposk', calories: 356, fat: 16.0, carbs: 49, protein: 3.9, iron: '16%' },
+        { name: 'q[spl', calories: 356, fat: 16.0, carbs: 49, protein: 3.9, iron: '16%' },
+        { name: '[pol]', calories: 356, fat: 16.0, carbs: 49, protein: 3.9, iron: '16%' },
+        { name: ';osjdiw', calories: 356, fat: 16.0, carbs: 49, protein: 3.9, iron: '16%' },
+        { name: 'poppi', calories: 356, fat: 16.0, carbs: 49, protein: 3.9, iron: '16%' },
+        { name: 'poiyt', calories: 356, fat: 16.0, carbs: 49, protein: 3.9, iron: '16%' },
+        { name: '[qslq]', calories: 356, fat: 16.0, carbs: 49, protein: 3.9, iron: '16%' },
+        { name: ';lsqpsk', calories: 356, fat: 16.0, carbs: 49, protein: 3.9, iron: '16%' },
+        { name: 'qlqpkws', calories: 356, fat: 16.0, carbs: 49, protein: 3.9, iron: '16%' },
+        { name: 'eqrwe', calories: 356, fat: 16.0, carbs: 49, protein: 3.9, iron: '16%' },
+        { name: 'gsyx', calories: 356, fat: 16.0, carbs: 49, protein: 3.9, iron: '16%' },
+        { name: 'vshbvc', calories: 356, fat: 16.0, carbs: 49, protein: 3.9, iron: '16%' },
+        { name: 'alsj', calories: 356, fat: 16.0, carbs: 49, protein: 3.9, iron: '16%' }
+      ]
+    }
+  }
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,73 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <v-card ref="form" width="500px" height="330px" class="mx-auto mt-16">
+      <v-card-title >login <i class="ml-3">{{ errorMessage }}</i></v-card-title>
+      <v-card-text>
+        <v-text-field
+          label="login" v-model="addName"
+          placeholder="input login" outlined dense
+          :rules="nameRules" :counter="15">
+        </v-text-field>
+        <v-text-field
+          :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+          label="Password" v-model="password"
+          placeholder="input password" outlined dense
+          :rules="lastNameRUles" :counter="10"
+          @click:append="show = !show" :type="show ? 'text' : 'password'">
+        </v-text-field>
+        <v-checkbox v-model="Disabled"
+          color="blue accent-2" label="Do you agree?"
+          class="ma-0">
+        </v-checkbox>
+        <div class="btns">
+          <v-btn text @click="reset">Clear</v-btn>
+          <v-btn text :disabled= "!Disabled" @click="openPage">submit</v-btn>
+        </div>
+      </v-card-text>
+    </v-card>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      errorMessage: '',
+      valid: true,
+      show: false,
+      addName: '',
+      password: '',
+      Disabled: false,
+      nameRules: [v => !!v || 'bu joyni to\'ldiring',
+        v => (!!v && v.length <= 15) || '15tagacha belgidan foydalanishingiz mumkin',
+        v => (!!v && v.length >= 4) || '4tadan ko\'p belgidan foydalanishingiz kerak'
+      ],
+      lastNameRUles: [
+        v => !!v || 'bu joyni to\'ldiring',
+        v => (!!v && v.length <= 10) || '10tagacha belgidan foydalanishingiz mumkin',
+        v => (!!v && v.length >= 4) || '4tadan ko\'p belgidan foydalanishingiz kerak'
+      ]
+    }
+  },
+  methods: {
+    openPage () {
+      if (this.password === 'salom') {
+        this.errorMessage = ''
+        this.$router.push({ name: 'about' })
+      } else {
+        this.errorMessage = 'Xato!'
+      }
+      this.Disabled = false
+    },
+    reset () {
+      this.addName = ''
+      this.password = ''
+      this.Disabled = false
+      this.$refs.form.reset()
+    }
   }
 }
+
 </script>
